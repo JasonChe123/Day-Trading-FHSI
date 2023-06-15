b21e7f51572217a932546d694fcb2a7325f00930
Jason Che <kcche2001@yahoo.com.hk>
2023-06-15 11:54:36 +0100

    Adding ANSI escape code remover

--- main.py
+++ main.py
@@ -22,3 +22,23 @@ from kivy.logger import Logger
 from kivy.uix.carousel import Carousel
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+
+
+class AnsiEscapeCodeRemover:
+    """
+    A custom file-like object that removes ANSI escape codes from text before writing to a file.
+    Use for writing log/ printed text to a .txt file rather than a terminal.
+    """
+
+    def __init__(self, file):
+        self.file = file
+
+    def write(self, text):
+        # Remove ANSI escape codes from the text
+        text = re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', '', text)
+        # Write the modified text to the file
+        self.file.write(text)
+
+    def flush(self):
+        self.file.flush()
+
