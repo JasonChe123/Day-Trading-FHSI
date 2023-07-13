cd2159cda78b560058ba11571b01da6fb694eb58
Jason Che <kcche2001@yahoo.com.hk>
2023-07-13 15:23:24 +0100

    Auto login to IBTWS.

--- library/programme.py
+++ library/programme.py
@@ -53,11 +53,10 @@ def launch_tws(project_dir, user_name: str, password: str):
     # search 2-factors authentication
     img_path = os.path.join(project_dir, 'library', 'images', 'ib_2_factors_authentication.png')
     while True:
-        element = search_image_on_screen(img_path, confidence=0.9, grayscale=True)
+        element = search_image_on_screen(img_path, confidence=0.5, grayscale=True)
         if element:
             break
         logging.info("IB TWS 2 factors authentication not found, try again after 5 seconds.")
-        print("IB TWS 2 factors authentication not found, try again after 5 seconds.")
         time.sleep(5)
 
     # simulate clicking event
@@ -68,7 +67,7 @@ def launch_tws(project_dir, user_name: str, password: str):
     auto_gui.moveTo((left, top + height), duration=0.2, tween=auto_gui.easeInOutQuad)
     auto_gui.moveTo((left, top), duration=0.2, tween=auto_gui.easeInOutQuad)
     x = left + width*5/6
-    y = top + height*15/16
+    y = top + height*9/10
     auto_gui.moveTo((x, y), duration=0.2, tween=auto_gui.easeInOutQuad)
     auto_gui.click()
 
@@ -84,7 +83,7 @@ def is_running(programme_name: str = '') -> bool:
 
 
 def search_image_on_screen(img_path: os.path, confidence: float, grayscale: bool,
-                           max_scale: float = 1.5, scale_factor: float = 0.01):
+                           max_scale: float = 2.0, scale_factor: float = 0.01):
     # open image, define size
     image = Image.open(img_path)
     image_width, image_height = image.size
@@ -93,26 +92,25 @@ def search_image_on_screen(img_path: os.path, confidence: float, grayscale: bool
 
     # search
     vector = 1  # to define searching for enlarged size or reduced size
-    tried = 0
     while True:
         logging.info(f"Searching {os.path.basename(img_path)}, size: {searching_w}x{searching_h}")
-        print(f"Searching {os.path.basename(img_path)}, size: {image.size}")
         element = auto_gui.locateOnScreen(image, confidence=confidence, grayscale=grayscale)
         if element:
-            print(f"{image} found.")
             return element
 
         # searching for different sizes
-        tried += 1
-        searching_w = image_width * (1 + tried*scale_factor*vector)
-        searching_h = image_height * (1 + tried*scale_factor*vector)
+        if vector == 1:
+            searching_w *= 1 + scale_factor
+            searching_h *= 1 + scale_factor
+        else:
+            searching_w *= 1 - scale_factor
+            searching_h *= 1 - scale_factor
 
         # if searching size is too large
         if vector == 1 and any([searching_w >= screen_width,
                                 searching_h >= screen_height,
                                 searching_w >= image_width * max_scale]):
             vector = -1  # search for reduced size
-            tried = 0
             # reset size
             searching_w = image_width
             searching_h = image_height
